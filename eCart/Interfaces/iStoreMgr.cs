03989d38a171fff0b2b9f65cfef38f8d1982b50e
Abel Salvatierra <assalvatierra@gmail.com>
2020-04-25 10:17:06 +0800

    added interface class - unfinished

---
+++ eCart/Interfaces/iStoreMgr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCart.Interfaces
+{
+    interface iStoreMgr
+    {
+        int getStoreDetails(int id);
+        int getStoreItems(int id);
+        int getPickupPoint(int id);
+
+
+    }
+}
