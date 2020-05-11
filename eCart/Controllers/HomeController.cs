39493cccd79369bf20d719a497c7a791b46dbfdc
Abel Salvatierra <assalvatierra@gmail.com>
2020-05-11 11:38:12 +0800

    login - adding param for usertype

--- eCart/Controllers/HomeController.cs
+++ eCart/Controllers/HomeController.cs
@@ -83,9 +83,14 @@ namespace eCart.Controllers
         //Authentication
 
         private readonly ecartdbContainer _dbContext = new ecartdbContainer();
-
-        public ActionResult Login()
+        /// <summary>
+        /// Login function 
+        /// </summary>
+        /// <param name="usertype">Integer: 15-admin, 1-Store, 0 or 2-shopper, 3-rider </param>
+        /// <returns></returns>
+        public ActionResult Login(int usertype)
         {
+            ViewBag.UserType = usertype; //use for altering the login page info/images and other info to fit the type of user
             return View();
         }
         [HttpPost]
